a2bd554ae7316a797324815fad8f695a80f3f091
MikeGoz <gozdz@onet.pl>
2017-10-11 16:33:20 +0200

    Task 5.5 modified

--- server_old.js
+++ server_old.js
@@ -1,35 +1,32 @@
-//Task 5.5 - modified
+//Task 5.5
 
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var reqNum = 0, //requests counter
-    reqData ='Requests registry : ',
-    timeFormatted;
+var reqNum = 0; //requests counter
+var reqData;
 
-var registry = function (req, res, next) {
+app.listen(3000);
+
+app.use('/:reqAdd', function(req, res, next){ //middleware registry 
   var time = new Date();
-  timeFormatted = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+  var timeFormatted = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
   reqNum += 1;
   console.log('Request number : ' + reqNum);
   console.log('Request time : ' + timeFormatted);
   console.log('Request address : ' + req.params.reqAdd); 
   console.log('Request type : '+ req.method);
   console.log('-------------------------');
-  next()
-};
-var registryToFile = function (req, res, next) {
+  
   reqData += "\r\n" + reqNum + " request: " + req.params.reqAdd + " | time: " + timeFormatted + " | method: " + req.method + " | ";  
   fs.writeFile('./registry.txt', reqData, function(err) {
     if (err) throw err;
   });    
-  next()
-};
-app.use("/:reqAdd", registry, registryToFile); 
+  next();
+});
 app.all('/store', function (req, res) {
   res.send('Welcome to our store');
 });
 app.use(function (req, res, next) {
   res.status(404).send('Sorry, wrong request...')
 });
-app.listen(3000);
